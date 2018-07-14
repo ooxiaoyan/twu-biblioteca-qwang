43b508042dd311c941d661c502780f8d863753c2
ooxiaoyan <810978527@qq.com>
2018-07-14 16:42:43 +0800

    feat(entity): add toString to Book

--- src/com/twu/biblioteca/entity/Book.java
+++ src/com/twu/biblioteca/entity/Book.java
@@ -1,11 +1,14 @@
 package com.twu.biblioteca.entity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by 筱湮 on 2018/7/14 0014.
  */
 public class Book {
 
-    private Integer id;
+    private String id;
     private String name;
     private String author;
     private String yearOfPublication;
@@ -13,14 +16,14 @@ public class Book {
 
     public Book() {}
 
-    public Book(Integer id, String name, String author, String yearOfPublication) {
+    public Book(String id, String name, String author, String yearOfPublication) {
         this.id = id;
         this.name = name;
         this.author = author;
         this.yearOfPublication = yearOfPublication;
     }
 
-    public Integer getId() {
+    public String getId() {
         return id;
     }
 
@@ -42,5 +45,16 @@ public class Book {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("id", this.id);
+        map.put("name", this.name);
+        map.put("author", this.author);
+        map.put("yearOfPublication", this.yearOfPublication);
+        map.put("status", this.status);
+        return map.toString();
     }
 }
