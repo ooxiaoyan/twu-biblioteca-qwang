3958b490b06cdafd6682a3b0631b168dbb0158f5
ooxiaoyan <810978527@qq.com>
2018-07-14 15:24:08 +0800

    feat(controller): add Welcome Message to BookController

---
+++ src/com/twu/biblioteca/controller/BookController.java
@@ -0,0 +1,11 @@
+package com.twu.biblioteca.controller;
+
+/**
+ * Created by 筱湮 on 2018/7/14 0014.
+ */
+public class BookController {
+
+    public void welcomeMessage() {
+        System.out.println("Welcome to Biblioteca!");
+    }
+}
