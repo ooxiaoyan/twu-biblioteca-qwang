f1aef4497a83783219025af79a284583d81cb86a
ooxiaoyan <810978527@qq.com>
2018-07-15 00:08:17 +0800

    feat(entity): add equals and hashCode method to Book

--- src/com/twu/biblioteca/entity/Book.java
+++ src/com/twu/biblioteca/entity/Book.java
@@ -58,4 +58,29 @@ public class Book {
         map.put("status", this.status);
         return map.toString();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (id != null ? !id.equals(book.id) : book.id != null) return false;
+        if (name != null ? !name.equals(book.name) : book.name != null) return false;
+        if (author != null ? !author.equals(book.author) : book.author != null) return false;
+        if (yearOfPublication != null ? !yearOfPublication.equals(book.yearOfPublication) : book.yearOfPublication != null)
+            return false;
+        return status != null ? status.equals(book.status) : book.status == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + (yearOfPublication != null ? yearOfPublication.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        return result;
+    }
 }
