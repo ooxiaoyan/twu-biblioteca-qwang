34632703c97ad8b9aac6acdee2b2cf8e09bc5705
ooxiaoyan <810978527@qq.com>
2018-07-15 16:38:02 +0800

    feat(controller): add checkoutMovieMenu method and modify return type on checkoutMovie method

--- src/com/twu/biblioteca/controller/MovieController.java
+++ src/com/twu/biblioteca/controller/MovieController.java
@@ -3,6 +3,8 @@ package com.twu.biblioteca.controller;
 import com.twu.biblioteca.datasource.MovieDataProvider;
 import com.twu.biblioteca.entity.Movie;
 
+import java.util.Scanner;
+
 /**
  * Created by 筱湮 on 2018/7/15 0015.
  */
@@ -10,14 +12,23 @@ public class MovieController {
 
     MovieDataProvider movieDataProvider = new MovieDataProvider();
 
-    public void checkoutMovie(String movieId) {
+    public Movie checkoutMovieMenu() {
+        System.out.print("\nPlease enter the movie id which you want to checkout: ");
+        Scanner input = new Scanner(System.in);
+        String movieId = input.next();
+        return checkoutMovie(movieId);
+    }
+
+    public Movie checkoutMovie(String movieId) {
         Movie movie = movieDataProvider.getMovie(movieId);
         if (movie != null && movie.getStatus().equals("1")) {
             int index = movieDataProvider.getMovies().indexOf(movie);
             movieDataProvider.getMovies().get(index).setStatus("0");
             successfulCheckout();
+            return movie;
         } else {
             unsuccessfulCheckout();
+            return null;
         }
     }
 
