cfd7fff68df12684cf67fa672f4a9e4ff539f373
ooxiaoyan <810978527@qq.com>
2018-07-15 13:54:30 +0800

    feat(entity): add User

---
+++ src/com/twu/biblioteca/entity/User.java
@@ -0,0 +1,72 @@
+package com.twu.biblioteca.entity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by 筱湮 on 2018/7/15 0015.
+ */
+public class User {
+
+    private String number;
+    private String password;
+    private String name;
+    private String email;
+    private String phone;
+    private boolean loginStatus;
+
+    private List checkoutList = new ArrayList();
+
+    public User() {
+    }
+
+    public User(String number, String password, String name, String email, String phone) {
+        this.number = number;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.loginStatus = false;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public boolean isLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(boolean loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+
+    @Override
+    public String toString() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("number", this.number);
+        map.put("password", this.password);
+        map.put("name", this.name);
+        map.put("email", this.email);
+        map.put("phone", this.phone);
+        return map.toString();
+    }
+}
