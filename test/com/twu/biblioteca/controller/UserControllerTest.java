811f3510a5b88759de721de826956ff6510cd13e
ooxiaoyan <810978527@qq.com>
2018-07-15 15:28:00 +0800

    test(controller): add test about print user information

--- test/com/twu/biblioteca/controller/UserControllerTest.java
+++ test/com/twu/biblioteca/controller/UserControllerTest.java
@@ -3,16 +3,29 @@ package com.twu.biblioteca.controller;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by 筱湮 on 2018/7/15 0015.
  */
 public class UserControllerTest {
 
     private UserController userController;
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Before
     public void setup() {
         userController = new UserController();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    private String systemOut() {
+        return outContent.toString();
     }
 
     @Test
@@ -21,5 +34,17 @@ public class UserControllerTest {
         String password = "0001";
         boolean result = userController.login(number, password);
         assertTrue(result);
+    }
+
+    @Test
+    public void should_print_user_information_when_user_logged_in() {
+        String userInfo =
+                "Name                                    Email                                             Phone               \n" +
+                "Kathy Sierra                            Sierra@gmail.com                                  15348673587 ";
+
+        userController.login("bpl-0001", "0001");
+        userController.printUserInfo();
+
+        assertThat(systemOut(), containsString(userInfo));
     }
 }
